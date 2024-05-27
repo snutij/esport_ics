5e3c6651358db14345b1952152649daf0a48907f
snutij <justin.rabiller@doctolib.com>
2024-05-27 23:49:18 +0200

    feat: add all missing leagues

--- lib/esport_ics/league_of_legends/league.rb
+++ lib/esport_ics/league_of_legends/league.rb
@@ -5,23 +5,43 @@ module EsportIcs
     module League
       LFL_ID = "74452834262590"
       LEC_ID = "97530759608318"
-      EMEA_MASTERS_ID = "51200282806521"
-      LCS_ID = "34247741770488"
       LPL_ID = "31698836969528"
       LCK_ID = "97692597116075"
+      LCS_ID = "34247741770488"
+      LVP_SL = "88501261250050"
+      EMEA_MASTERS_ID = "51200282806521"
+      PRIME_LEAGUE_ID = "70651812955729"
       LFL_DIV2_ID = "72652091833775"
+      CBLOL_ID = "27530694587361"
+      NLC_ID = "72759581872364"
+      PCS_ID = "82197302942007"
       MSI_ID = "69220076243247"
+      LCK_CHALLENGER_ID = "31477133884165"
+      ULTRA_LIGUA_ID = "66173784145627"
+      LVP_SL_DIV2_ID = "11361001793149"
+      PG_NATIONALS_ID = "37452779855159"
+      TCL_ID = "15779874788282"
       WORLDS_ID = "50606172015690"
 
       ALL = [
         LFL_ID,
         LEC_ID,
-        EMEA_MASTERS_ID,
-        LCS_ID,
         LPL_ID,
         LCK_ID,
+        LCS_ID,
+        LVP_SL,
+        EMEA_MASTERS_ID,
+        PRIME_LEAGUE_ID,
         LFL_DIV2_ID,
+        CBLOL_ID,
+        NLC_ID,
+        PCS_ID,
         MSI_ID,
+        LCK_CHALLENGER_ID,
+        ULTRA_LIGUA_ID,
+        LVP_SL_DIV2_ID,
+        PG_NATIONALS_ID,
+        TCL_ID,
         WORLDS_ID,
       ].freeze
     end
