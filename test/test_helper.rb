896873a2c2124ac6dac801a461b1bad66c66cae8
snutij <justin.rabiller@doctolib.com>
2024-05-30 13:36:30 +0200

    test: add webmock to test_helper and constant for fixtures and expectations paths

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,11 @@ $LOAD_PATH.unshift(File.expand_path("../lib", __dir__))
 require "esport_ics"
 
 require "minitest/autorun"
+require "webmock/minitest"
 require "debug"
+
+FIXTURES_PATH = File.expand_path("fixtures", __dir__)
+EXPECTATIONS_PATH = File.expand_path("expectations", __dir__)
 
 class FakeHTTPResponse
   attr_reader :code, :body
