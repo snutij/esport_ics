8c5c3d9fb4b2065109b860d1370be130b65998d9
snutij <justin.rabiller@doctolib.com>
2024-07-03 10:50:55 +0200

    test: remove code duplication

--- test/league_of_legends/generator_test.rb
+++ test/league_of_legends/generator_test.rb
@@ -5,11 +5,10 @@ require "test_helper"
 module EsportIcs
   module LeagueOfLegends
     class GeneratorTest < Minitest::Test
-      MOCK_MATCHES = File.read(File.join(FIXTURES_PATH, "league_of_legends", "matches.json"))
       EXPECTED_ICS = Dir.glob(File.join(EXPECTATIONS_PATH, "league_of_legends", "*.ics")).map { |f| File.read(f) }
 
       def test_create_ics
-        stub_matches_league do
+        stub_matches_league(Generator::GAME_SLUG) do
           calendars = Generator.new.generate.calendars.values.map(&:to_ical)
 
           assert_equal(calendars.size, EXPECTED_ICS.size)
@@ -18,40 +17,6 @@ module EsportIcs
             .group_by { |c| c.custom_property("slug").first }
             .each { |_slug, (cal, exp)| assert_same_calendar(cal, exp) }
         end
-      end
-
-      private
-
-      def assert_same_calendar(calendar, expected_ics)
-        assert(calendar)
-        assert(expected_ics)
-
-        assert_equal(calendar.custom_property("name"), expected_ics.custom_property("name"))
-        assert_equal(calendar.custom_property("slug"), expected_ics.custom_property("slug"))
-        assert_equal(calendar.ip_name, expected_ics.ip_name)
-        assert_equal(calendar.ip_method, expected_ics.ip_method)
-        assert_equal(calendar.events.size, expected_ics.events.size)
-
-        calendar.events.zip(expected_ics.events).each do |event, expected_event|
-          assert_equal(event.summary, expected_event.summary)
-          assert_equal(event.ip_class, expected_event.ip_class)
-          assert_equal(event.dtstart.to_s, expected_event.dtstart.to_s)
-          assert_equal(event.dtend.to_s, expected_event.dtend.to_s)
-        end
-      end
-
-      def stub_matches_league
-        stub_request(
-          :get,
-          "#{Api.new(game_slug: Generator::GAME_SLUG).matches_url}?page[size]=100&page[number]=1",
-        ).to_return_json(body: JSON.parse(MOCK_MATCHES).to_json)
-
-        stub_request(
-          :get,
-          "#{Api.new(game_slug: Generator::GAME_SLUG).matches_url}?page[size]=100&page[number]=2",
-        ).to_return_json(body: [].to_json)
-
-        yield
       end
     end
   end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,3 +10,37 @@ require "webmock/minitest"
 
 FIXTURES_PATH = File.expand_path("fixtures", __dir__)
 EXPECTATIONS_PATH = File.expand_path("expectations", __dir__)
+
+def assert_same_calendar(calendar, expected_ics)
+  assert(calendar)
+  assert(expected_ics)
+
+  assert_equal(calendar.custom_property("name"), expected_ics.custom_property("name"))
+  assert_equal(calendar.custom_property("slug"), expected_ics.custom_property("slug"))
+  assert_equal(calendar.ip_name, expected_ics.ip_name)
+  assert_equal(calendar.ip_method, expected_ics.ip_method)
+  assert_equal(calendar.events.size, expected_ics.events.size)
+
+  calendar.events.zip(expected_ics.events).each do |event, expected_event|
+    assert_equal(event.summary, expected_event.summary)
+    assert_equal(event.ip_class, expected_event.ip_class)
+    assert_equal(event.dtstart.to_s, expected_event.dtstart.to_s)
+    assert_equal(event.dtend.to_s, expected_event.dtend.to_s)
+  end
+end
+
+def stub_matches_league(game_slug)
+  mock_matches = File.read(File.join(FIXTURES_PATH, game_slug, "matches.json"))
+
+  stub_request(
+    :get,
+    "#{EsportIcs::Api.new(game_slug: game_slug).matches_url}?page[size]=100&page[number]=1",
+  ).to_return_json(body: JSON.parse(mock_matches).to_json)
+
+  stub_request(
+    :get,
+    "#{EsportIcs::Api.new(game_slug: game_slug).matches_url}?page[size]=100&page[number]=2",
+  ).to_return_json(body: [].to_json)
+
+  yield
+end
