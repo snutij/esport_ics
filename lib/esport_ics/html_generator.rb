dc72bc931fc08c871afa39e7a712343e67e77df6
Justin Rabiller <38727166+snutij@users.noreply.github.com>
2026-02-07 08:35:54 +0100

    feat: add GitHub logo link in header (#99)

--- lib/esport_ics/html_generator.rb
+++ lib/esport_ics/html_generator.rb
@@ -110,6 +110,19 @@ module EsportIcs
             margin-bottom: 2rem;
             padding-bottom: 2rem;
             border-bottom: 1px solid #30363d;
+            position: relative;
+          }
+
+          .github-link {
+            position: absolute;
+            top: 0;
+            right: 0;
+            color: #8b949e;
+            transition: color 0.2s;
+          }
+
+          .github-link:hover {
+            color: #e6edf3;
           }
 
           h1 {
@@ -434,6 +447,11 @@ module EsportIcs
     def generate_header
       <<~HTML
         <header>
+          <a href="https://github.com/snutij/esport_ics" target="_blank" class="github-link" title="View on GitHub">
+            <svg width="32" height="32" viewBox="0 0 16 16" fill="currentColor">
+              <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"/>
+            </svg>
+          </a>
           <h1>Esport ICS Calendars</h1>
           <p class="subtitle">Subscribe to your favorite esports team schedules</p>
         </header>
