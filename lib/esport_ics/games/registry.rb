62b18d10a559e6abd9814dcd1b32161c17ad4aea
snutij <justin.rabiller@doctolib.com>
2026-02-10 22:37:40 +0100

    feat: add 6 new esport games from PandaScore API
    Add King of Glory, Mobile Legends, PUBG, EA Sports FC, StarCraft 2
    and StarCraft: Brood War. Reorder games by 2025 esports viewership.

--- lib/esport_ics/games/registry.rb
+++ lib/esport_ics/games/registry.rb
@@ -8,11 +8,17 @@ module EsportIcs
       CallOfDutyMW: { api_code: "codmw", ics_folder: "call_of_duty_mw" },
       CounterStrike: { api_code: "csgo", ics_folder: "counter_strike" },
       Dota2: { api_code: "dota2", ics_folder: "dota_2" },
+      EaSportsFc: { api_code: "fifa", ics_folder: "ea_sports_fc" },
+      KingOfGlory: { api_code: "kog", ics_folder: "king_of_glory" },
       LeagueOfLegends: { api_code: "lol", ics_folder: "league_of_legends" },
       LeagueOfLegendsWildRift: { api_code: "lol-wild-rift", ics_folder: "league_of_legends_wildrift" },
+      MobileLegends: { api_code: "mlbb", ics_folder: "mobile_legends" },
       Overwatch2: { api_code: "ow", ics_folder: "overwatch_2" },
+      Pubg: { api_code: "pubg", ics_folder: "pubg" },
       RainbowSixSiege: { api_code: "r6siege", ics_folder: "rainbow_six_siege" },
       RocketLeague: { api_code: "rl", ics_folder: "rocket_league" },
+      StarCraft2: { api_code: "starcraft-2", ics_folder: "starcraft_2" },
+      StarCraftBroodWar: { api_code: "starcraft-brood-war", ics_folder: "starcraft_brood_war" },
       Valorant: { api_code: "valorant", ics_folder: "valorant" },
     }.freeze
 
